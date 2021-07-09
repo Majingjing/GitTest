7dc33b11aa6e5c310e9b90e413eb047deafe27d7
麻静 <18211075822@139.com>
2021-07-09 10:01:48 +0800

    新增vc

---
+++ 111/111/MJViewController.h
@@ -0,0 +1,16 @@
+//
+//  MJViewController.h
+//  111
+//
+//  Created by 中电金信 on 2021/7/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 111/111/MJViewController.m
@@ -0,0 +1,31 @@
+//
+//  MJViewController.m
+//  111
+//
+//  Created by 中电金信 on 2021/7/9.
+//
+
+#import "MJViewController.h"
+
+@interface MJViewController ()
+
+@end
+
+@implementation MJViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
